395ba0a79bab57700123b0e48d54730a096d3ecf
Robert Stiff <robert.stiff@just-eat.com>
2018-05-04 22:30:07 +0100

    Metrics API implementation

--- src/Neutrino.Seyren/IMetrics.cs
+++ src/Neutrino.Seyren/IMetrics.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace Neutrino.Seyren
 {
     public interface IMetrics
     {
         // GET - /api/metrics/{target}/total
-        int GetMetricCount(string target);  
+        Task<Dictionary<string, long>> GetMetricCount(string target);  
+    }
+
+    public partial class SeyrenClient : IMetrics
+    {
+        public IMetrics Metrics => (IMetrics) this;
+
+        async Task<Dictionary<string, long>> IMetrics.GetMetricCount(string target)
+        {
+            string serialisedConfig = await this.httpClient.GetStringAsync($"/api/metrics/{target}/total");
+
+            return JsonConvert.DeserializeObject<Dictionary<string, long>>(serialisedConfig);
+        }
     }
 }
