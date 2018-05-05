52624eb99cc4c21e98d0f2742ad85b8ddbbde74d
Robert Stiff <robert.stiff@just-eat.com>
2018-05-05 11:58:00 +0100

    implementation documentation

--- src/Neutrino.Seyren/IMetrics.cs
+++ src/Neutrino.Seyren/IMetrics.cs
@@ -6,14 +6,17 @@ namespace Neutrino.Seyren
 {
     public interface IMetrics
     {
-        // GET - /api/metrics/{target}/total
         Task<Dictionary<string, long>> GetMetricCount(string target);  
     }
 
+    /// <summary>
+    /// https://github.com/scobal/seyren/blob/e40cdd0bf887ab1ca496fa9ca1772462c91cf263/seyren-api/src/main/java/com/seyren/api/bean/MetricsBean.java
+    /// </summary>
     public partial class SeyrenClient : IMetrics
     {
         public IMetrics Metrics => (IMetrics) this;
 
+        // GET - /api/metrics/{target}/total
         async Task<Dictionary<string, long>> IMetrics.GetMetricCount(string target)
         {
             string serialisedConfig = await this.httpClient.GetStringAsync($"/api/metrics/{target}/total");
